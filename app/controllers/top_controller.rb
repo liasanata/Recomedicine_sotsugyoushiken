3d7d3e97bd1ed2abe4bfbd3779d412f1c01c2a3f
liasanata <sanata354469@icloud.com>
2025-06-03 19:40:48 +0900

    flyioデプロイのため

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,15 +2,14 @@ class TopController < ApplicationController
 
   def index
     # もしリクエストがGETメソッドであれば、params[:problem_check]を確認する
-    if params[:problem_check]
+    if params[:problem_check].present?
       if params[:problem_check].include?('1')
         redirect_to controller: 'problems_check', action: 'recommendation_for_medical_examination' and return
       elsif params[:problem_check].include?('2')
         redirect_to controller: 'problems_check', action: 'precautions' and return
+      elsif params[:problem_check].include?('0')
+        redirect_to controller: 'symptoms', action: 'index' and return
       end
     end
-    # どちらの条件にも当てはまらなければ、症状入力画面を表示する
-    redirect_to controller: 'symptoms', action: 'index'
   end
-
 end
